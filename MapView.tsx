23435148ce10df6dfa88fb2294e203936e850585
david3tra-cloud <david3tra@gmail.com>
2026-02-16 21:53:07 +0100

    Fix: Correct import path from './_vpes' to './_types' in MapView.tsx
    This fixes the build error where MapView.tsx was trying to import from a non-existent './_vpes' file. The correct path is './_types' where the type definitions are located.

--- MapView.tsx
+++ MapView.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
-import { Delivery, DeliveryStatus, DeliveryType } from '../ypes';
+import { Delivery, DeliveryStatus, DeliveryType } from '../types';
 
 interface MapViewProps {
   deliveries: Delivery[];
